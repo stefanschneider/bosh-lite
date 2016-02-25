ed12ced1f7a18a41e71cc7d8ff99bb7de13f3e76
Stefan Schneider <stefan.schneider@hpe.com>
2016-02-25 17:10:37 +0200

    Add windows vagrant sample

---
+++ windows-cell/install-diego.ps1
@@ -0,0 +1,68 @@
+$wd="C:\diego-kit"
+mkdir -Force $wd
+cd $wd
+
+
+## Cleanup previous installations
+
+echo "Trying to uninstall DiegoWindows and GardenWindows"
+$diegoWindowsProduct = (gwmi Win32_Product -Filter "Name = 'DiegoWindows'")
+if ($diegoWindowsProduct) { $diegoWindowsProduct.Uninstall() }
+
+$gardenWindowsProduct = (gwmi Win32_Product  -Filter "Name = 'GardenWindows'")
+if ($gardenWindowsProduct) { $gardenWindowsProduct.Uninstall() }
+
+
+## Download installers
+
+$gardenReleaseVersion="v0.153"
+$diegoReleaseVersion="v0.416"
+curl -UseBasicParsing -OutFile $wd\setup.ps1 https://github.com/cloudfoundry/garden-windows-release/releases/download/$gardenReleaseVersion/setup.ps1 -Verbose
+curl -UseBasicParsing -OutFile $wd\GardenWindows.msi https://github.com/cloudfoundry/garden-windows-release/releases/download/$gardenReleaseVersion/GardenWindows.msi -Verbose
+curl -UseBasicParsing -OutFile $wd\DiegoWindows.msi https://github.com/cloudfoundry/diego-windows-release/releases/download/$diegoReleaseVersion/DiegoWindows.msi -Verbose
+curl -UseBasicParsing -OutFile $wd\generate.exe https://github.com/cloudfoundry/diego-windows-release/releases/download/$diegoReleaseVersion/generate.exe -Verbose
+curl -UseBasicParsing -OutFile $wd\hakim.exe https://github.com/cloudfoundry/diego-windows-release/releases/download/$diegoReleaseVersion/hakim.exe -Verbose
+
+
+# Dependencies
+
+# Install chocolatey
+iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# Make sure powershell is installed
+Install-WindowsFeature PowerShell, PowerShell-V2 
+
+# Deps for cf-iis8-buildpack
+Install-WindowsFeature Web-Net-Ext, Web-AppInit 
+choco install vcredist2012 -y
+choco install vcredist2013 -y
+choco install vcredist2015 -y
+
+## Setup diego networking
+
+$machineIp = (Find-NetRoute -RemoteIPAddress "192.168.50.4")[0].IPAddress
+$diegoInterface = Get-NetIPAddress -IPAddress $machineIp
+# $diegoInterface | Remove-NetIPAddress -AddressFamily IPv4 -Confirm:$false
+# $diegoInterface | New-NetIPAddress -AddressFamily IPv4  -IPAddress $machineIp -PrefixLength $diegoInterface.PrefixLength
+
+### 1.2.3.4 is used by rep to discover the IP address to be announced to the diego cluster
+route delete 1.2.3.4
+route add 1.2.3.4 192.168.50.4 -p
+
+route delete 10.244.0.0
+route add 10.244.0.0 mask 255.255.0.0 192.168.50.4 -p
+
+
+## Setup diego
+
+echo "Running setup.ps1"
+powershell $wd\setup.ps1 -quiet
+
+echo "Running generate.exe"
+& $wd\generate.exe  -boshUrl https://admin:admin@192.168.50.4:25555  -outputDir .  -machineIp "$machineIp"
+
+echo "Installing DiegoWindows and GardenWindows"
+& $wd\install.bat
+
+echo "Running hakim to check windows cell status"
+& $wd\hakim.exe
---
+++ windows-cell/rearm.ps1
@@ -0,0 +1,17 @@
+﻿get-ciminstance -class SoftwareLicensingProduct |
+  where {$_.name -match 'windows' -AND $_.licensefamily} |   where {$_.LicenseStatus -ne 0} |
+    format-list -property Name, Description, `
+             @{Label=”Grace period (days)”; Expression={ $_.graceperiodremaining / 1440}}, `
+             @{Label= “License Status”; Expression={switch (foreach {$_.LicenseStatus}) `
+              { 0 {“Unlicensed”} `
+                1 {“Licensed”} `
+                2 {“Out-Of-Box Grace Period”} `
+                3 {“Out-Of-Tolerance Grace Period”} `
+                4 {“Non-Genuine Grace Period”} `
+                5 {“Notification Period”} `
+                6 {“Extended Grace Period”} `
+              } } }
+
+
+(Get-WmiObject SoftwareLicensingService).ReArmWindows()
+echo "Reboot Windows. Tip: PS command for reboot: Restart-Computer"
